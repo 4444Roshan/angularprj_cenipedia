7510042a675e231f6007fc0628a843bd0a8f34f1
4444Roshan <77161253+4444Roshan@users.noreply.github.com>
2024-04-23 16:13:09 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+
+const appRoute: Routes =[
+  {path:'', component:HomeComponent},
+  {path:"About/:id", component:AboutComponent},
+  {path:"About", component:AboutComponent},
+  {path:"form/:id", component:FormComponent},
+  {path:"form",component:FormComponent}
+
+]
+@NgModule({
+  imports: [RouterModule.forRoot(appRoute)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FilterComponent } from './filter/filter.component';
+import { AboutComponent } from './about/about.component';
+import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FilterComponent,
+    AppComponent,
+    FormComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
